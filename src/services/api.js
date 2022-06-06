dfaa55a07c53f33266b80b519f0d14897c32e907
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-06-06 15:37:18 -0300

    feat: include password hard

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-export const createEvent = async ({ name, finishTime, ip }) => {
+export const createEvent = async ({
+  name,
+  finishTime,
+  ip,
+  password = "ignored",
+}) => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}/event`, {
     method: "POST",
     headers: {
       accept: "application/json",
       "content-type": "application/json",
     },
-    body: JSON.stringify({ name, finishTime, ip }),
+    body: JSON.stringify({ name, finishTime, ip, password }),
   });
   const data = await response.json();
   return data;
